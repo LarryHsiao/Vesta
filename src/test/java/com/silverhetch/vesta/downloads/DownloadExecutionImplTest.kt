b6391666cca4484ec4498f9d608b437d1dedf37a
Larry Hsiao <larryhsiao@silverhetch.com>
2019-02-14 14:10:34 +0800

    check db entry size in DownloadExecutionImplTest.kt

--- src/test/java/com/silverhetch/vesta/downloads/DownloadExecutionImplTest.kt
+++ src/test/java/com/silverhetch/vesta/downloads/DownloadExecutionImplTest.kt
@@ -8,30 +8,29 @@ import java.net.URI
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.*
 
 class DownloadExecutionImplTest {
     @Test
     fun downloadTest() {
+        val downloads = DBDownloads(
+            H2DB(Files.createTempDirectory("temp").toFile().absolutePath).connection()
+        ).apply {
+            init()
+            new(URI("https://dummyimage.com/600x400/000/fff"))
+            new(URI("https://dummyimage.com/600x400/111/eee"))
+            new(URI("https://dummyimage.com/600x400/222/ddd"))
+        }
         val countDownLatch = CountDownLatch(3)
         val targetDir = Files.createTempDirectory("downloadExecutionImplTest")
             .toFile()
             .apply { deleteOnExit() }
-        DownloadExecutionImpl(
-            DBDownloads(
-                H2DB(Files.createTempDirectory("temp").toFile().absolutePath).connection()
-            ).apply {
-                init()
-                new(URI("https://dummyimage.com/600x400/000/fff"))
-                new(URI("https://dummyimage.com/600x400/111/eee"))
-                new(URI("https://dummyimage.com/600x400/222/ddd"))
-            }, targetDir
-        ).start { doneUri ->
+        DownloadExecutionImpl(downloads, targetDir).start { doneUri ->
             Assert.assertTrue(File(targetDir, Paths.get(doneUri).fileName.toString()).exists())
             countDownLatch.countDown()
         }
         countDownLatch.await(10000, MILLISECONDS)
         Assert.assertEquals(0, countDownLatch.count)
+        Assert.assertEquals(0, downloads.all().size)
     }
 }
