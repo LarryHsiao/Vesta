714711aba764edf06b7ca70a79beff62e60b5018
Larry Hsiao <larryhsiao@silverhetch.com>
2019-02-14 23:20:14 +0800

    todo for tag management

---
+++ src/main/java/com/silverhetch/vesta/jfx/TagManagementView.kt
@@ -0,0 +1,13 @@
+package com.silverhetch.vesta.jfx
+
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+class TagManagementView : Initializable {
+    override fun initialize(p0: URL?, p1: ResourceBundle?) {
+        /**
+         * @todo #5 Implementation of Tag management.
+         */
+    }
+}
